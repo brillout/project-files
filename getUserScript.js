551f5b24360000d8500c10797b3375bbe9f422d5
Romuald Brillout <github.com@brillout.com>
2019-09-30 21:12:43 +0200

    improve DEBUG logging

--- getUserScript.js
+++ getUserScript.js
@@ -1,9 +1,6 @@
 const pathModule = require('path');
 const assert = require('@brillout/reassert');
 
-// We get the callstack now to make sure we don't get the callstack of an event loop
-const callstack = getV8StackTrace();
-
 /*
 const DEBUG = true;
 /*/
@@ -13,41 +10,22 @@ const DEBUG = false;
 module.exports = getUserScript;
 
 function getUserScript() {
-    const stackPaths = getStackPaths();
-    if(DEBUG) console.log('stack trace', stackPaths);
-    for( let i = 0; i<stackPaths.length; i++ ){
-      const filePath = stackPaths[i];
+    const callstack = getCallstack();
+    if(DEBUG) console.log('stack trace', callstack);
+    for( let i = 0; i<callstack.length; i++ ){
+      const filePath = callstack[i];
+      if( isDependency(filePath) ){
+        continue;
+      }
       const is_bin_call = isBinCall(filePath);
       if(DEBUG) console.log('is bin call', filePath, is_bin_call);
       if( is_bin_call ){
-        assert.internal(i===0, {filePath, stackPaths});
+        assert.internal(i===0, {filePath, callstack});
         return null;
       }
       return filePath;
     }
     return null;
-}
-
-function getStackPaths() {
-  const stackPaths = [];
-  for( let i = callstack.length-1; i>=0; i-- ){
-    const call = callstack[i];
-    if( call.isNative() ){
-      continue;
-    }
-    const filePath = call.getFileName();
-    if( ! filePath ){
-      continue;
-    }
-    if( isNode(filePath) ){
-      continue;
-    }
-    if( isDependency(filePath) ){
-      continue;
-    }
-    stackPaths.push(filePath);
-  }
-  return stackPaths;
 }
 
 function getFileProjectFiles(filePath) {
@@ -75,19 +53,40 @@ function isBinCall(filePath) {
   const p2 = require.resolve(filePath);
   return p1===p2;
 }
+
+function getCallstack() {
+  const callstack = [];
+  for( let i = callstack_raw.length-1; i>=0; i-- ){
+    const call = callstack_raw[i];
+    if( call.isNative() ){
+      continue;
+    }
+    const filePath = call.getFileName();
+    if( ! filePath ){
+      continue;
+    }
+    if( isNode(filePath) ){
+      continue;
+    }
+    callstack.push(filePath);
+  }
+  return callstack;
+}
 function isNode(filePath) {
     return !pathModule.isAbsolute(filePath);
 }
 function isDependency(filePath) {
     return filePath.split(pathModule.sep).includes('node_modules');
 }
-function getV8StackTrace() {
+// We get the callstack now to make sure we don't get the callstack of an event loop
+const callstack_raw = getRawCallstack();
+function getRawCallstack() {
     const callsites = require('callsites');
 
     const stackTraceLimit__original = Error.stackTraceLimit;
     Error.stackTraceLimit = Infinity;
-    const calls = callsites();
+    const callstack_raw = callsites();
     Error.stackTraceLimit = stackTraceLimit__original;
 
-    return calls;
+    return callstack_raw;
 }
